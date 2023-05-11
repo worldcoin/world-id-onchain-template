22ae20a4a18246cde699a3d7fb3fbca88f91aca2
Miguel Piedrafita <github@miguel.build>
2023-05-11 13:43:01 +0100

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
+import { APP_NAME } from '@/lib/consts'
 import type { AppProps } from 'next/app'
+import { WagmiConfig, createClient } from 'wagmi'
+import { ConnectKitProvider, getDefaultClient } from 'connectkit'
+
+const client = createClient(
+	getDefaultClient({
+		appName: APP_NAME,
+		infuraId: process.env.INFURA_ID,
+	})
+)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ConnectKitProvider>
+			<WagmiConfig client={client}>
+				<Component {...pageProps} />
+			</WagmiConfig>
+		</ConnectKitProvider>
+	)
 }
