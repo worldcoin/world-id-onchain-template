3ce54b3b32254cc7972486393a54da7eff57c825
JZ <justin.zheng@gmail.com>
2024-03-04 18:43:14 -0800

    Upgrade packages, add new dev portal instructions to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,22 @@ import { BigNumber } from 'ethers'
 import { decode } from '@/lib/wld'
 import ContractAbi from '@/abi/Contract.abi'
 import { ConnectKitButton } from 'connectkit'
-import { IDKitWidget, ISuccessResult } from '@worldcoin/idkit'
+import { IDKitWidget, ISuccessResult, VerificationLevel } from '@worldcoin/idkit'
 import { useAccount, useContractWrite, usePrepareContractWrite } from 'wagmi'
 
 export default function Home() {
 	const { address } = useAccount()
 	const [proof, setProof] = useState<ISuccessResult | null>(null)
 
+	const verifyProof = async (proof: ISuccessResult) => {
+		throw new Error("TODO: verify proof server route")
+	  };
+	  
+	  // TODO: Functionality after verifying
+	  const onSuccess = () => {
+		console.log("Success")
+	  };
+	  
 	const { config } = usePrepareContractWrite({
 		address: process.env.NEXT_PUBLIC_CONTRACT_ADDR as `0x${string}`,
 		abi: ContractAbi,
@@ -49,6 +58,8 @@ export default function Home() {
 						signal={address}
 						action="your-action"
 						onSuccess={setProof}
+						handleVerify={verifyProof}
+						verification_level={VerificationLevel.Orb}
 						app_id={process.env.NEXT_PUBLIC_APP_ID!}
 					>
 						{({ open }) => <button onClick={open}>verify with world id</button>}
